2575c39ab4cb6f5d1deebb4099d56dd3c8dd2b97
magflores <magflores@itba.edu.ar>
2022-05-20 14:59:46 -0300

    conexión firebase uwu

--- src/main.js
+++ src/main.js
@@ -3,34 +3,34 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
+import VueResource from 'vue-resource';
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore/lite';
 
+Vue.use(VueResource);
+
+Vue.http.options.root = 'petify-arg.firebaseapp.com'
 Vue.config.productionTip = false
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-// import firebase from "firebase/compat";
-//
-// Vue.http.options.root = 'petify-arg.firebaseapp.com'
-//
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBzs6LvWrghh3InI50ZGGmo5inQ1UAzU5w",
-//   authDomain: "petify-ce198.firebaseapp.com",
-//   databaseURL: "https://petify-ce198-default-rtdb.firebaseio.com",
-//   projectId: "petify-ce198",
-//   storageBucket: "petify-ce198.appspot.com",
-//   messagingSenderId: "335528341236",
-//   appId: "1:335528341236:web:21460374deb9a1e83d78a1",
-//   measurementId: "G-86ZT2PWKGM"
-// };
-//
-// // Initialize Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzs6LvWrghh3InI50ZGGmo5inQ1UAzU5w",
+  authDomain: "petify-ce198.firebaseapp.com",
+  databaseURL: "https://petify-ce198-default-rtdb.firebaseio.com",
+  projectId: "petify-ce198",
+  storageBucket: "petify-ce198.appspot.com",
+  messagingSenderId: "335528341236",
+  appId: "1:335528341236:web:21460374deb9a1e83d78a1",
+  measurementId: "G-86ZT2PWKGM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export{firestore};
 // firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
-// firebase.auth = getAuth();
 
 new Vue({
   vuetify,
