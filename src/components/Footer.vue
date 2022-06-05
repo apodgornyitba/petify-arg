7e333115bc941567bababab4531b248a9ccc0e55
cami <csierra@itba.edu.ar>
2022-06-04 21:59:48 -0300

    footer con about us

---
+++ src/components/Footer.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-footer
+      dark
+      height="auto"
+      elevation="0"
+      padless color="#2A537A"
+      class="justify-center"
+  >
+    <v-card
+        flat
+        tile
+        color="#2A537A"
+        class="white--text text-center"
+    >
+      <v-card-text class="white--text pt-1">
+        <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-3 white--text "
+            dark
+            icon
+        >
+          <v-icon dark size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        <v-row
+            align="center"
+            class="mx-3"
+        >
+<!--          <h4>Sobre nosotros:</h4>-->
+          Sobre nosotros:
+        </v-row>
+
+        <v-row
+            align="center"
+            class="mx-6"
+        >
+            Somos el sitio de animales rescatados de Argentina. Buscamos unificar los refugios de animales en un solo
+          sitio para agilizar y fomentar la adopción responsable.
+          <v-spacer></v-spacer>
+          En este sitio encontrarán cada refugio con el que nos asociamos y los respectivos animales (tanto perros como gatos)
+          que necesitan encontrar a su familia ideal. Hay formularios que completar para solicitar la adopción de una mascota, también
+          ofrecemos la posibilidad de ayudar a los refugios, así tambien la opción de denunciar comportamiento extraño.
+        </v-row>
+
+<!--        <v-row-->
+<!--            align="center"-->
+<!--            class="mx-0"-->
+<!--        >-->
+<!--          En este sitio encontrarán cada refugio con el que nos asociamos y los respectivos animales (tanto perros como gatos)-->
+<!--          que necesitan encontrar a su familia ideal. Hay formularios que completar para solicitar la adopción de una mascota, también-->
+<!--          ofrecemos la posibilidad de ayudar a los refugios, así tambien la opción de denunciar comportamiento extraño.-->
+<!--        </v-row>-->
+
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text justify-center">
+        &copy;2022 — <strong>Petify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+  data: () => ({
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-instagram'
+    ]
+  })
+}
+</script>
+
+<style>
+</style>
--- src/views/DenunciarView.vue
+++ src/views/DenunciarView.vue
@@ -3,12 +3,53 @@
   <div>
     <ToolBar/>
     <v-container>
-      <v-row class="align-center">
+      <v-row class="align-center" style="margin-top: 50px">
         <v-col>
           <h2>
             Formulario para denunciar comportamiento extraño de un refugio
           </h2>
         </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <h4 class="font-weight-medium">
+            Por favor complete el siguiente formulario para iniciar el trámite de denuncia de Patitas al Rescate.
+          </h4>
+        </v-col>
+      </v-row>
+      <v-row class="align-center">
+        <v-col cols="8">
+        <v-text-field
+          name="Motivo"
+          label="Motivo"
+          v-model="motivo"
+
+        >
+        </v-text-field>
+        </v-col>
+
+        <v-col cols="8">
+          <v-text-field
+              name="Motivo"
+              label="Mas campos a definir"
+              v-model="motivo"
+          >
+          </v-text-field>
+        </v-col>
+
+      </v-row>
+
+      <v-row>
+        <v-btn
+            class="mr-4"
+            type="submit"
+        >
+          Enviar
+        </v-btn>
+        <v-btn @click="clear">
+          Vaciar
+        </v-btn>
       </v-row>
     </v-container>
   </div>
@@ -18,8 +59,13 @@
 import ToolBar from "@/components/Toolbar";
 export default {
   name: "DenunciarView",
-  components: {ToolBar}
+  components: {ToolBar},
+  data: () => ({
+    motivo: '',
+
+  }),
 }
+
 </script>
 
 <style scoped>
